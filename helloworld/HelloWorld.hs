8946582a3eb5e57b6ef5add0140daeb143a2378f
Marek Materzok <tilk@tilk.eu>
2021-11-08 16:45:08 +0100

    Basic example

--- helloworld/HelloWorld.hs
+++ helloworld/HelloWorld.hs
@@ -6,8 +6,9 @@ import FSM.HD44780
 
 topEntity 
     :: "clk" ::: Clock System
+    -> "rst" ::: Reset System
     -> "hd44780" ::: Signal System HD44780_Input
-topEntity clk = pure empty_hd44780
+topEntity clk rst = exposeClockResetEnable (fst <$> hd44780 (pure $ Bus_Input False undefined undefined)) clk rst enableGen
 
 makeTopEntity 'topEntity
 
